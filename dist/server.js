31f6ed1d3294296905f56ea4be5d441221931e5b
Gabriel Costa <nevesgabriel10@gmail.com>
2026-03-26 13:51:09 -0300

    Server

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,10 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
+"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _dotenv = require('dotenv'); var _dotenv2 = _interopRequireDefault(_dotenv);
+_dotenv2.default.config();
+
+var _appjs = require('./app.js'); var _appjs2 = _interopRequireDefault(_appjs);
 
 const port = process.env.APP_PORT;
-_appjs2.default.listen(port);
+
+_appjs2.default.listen(port, () => {
+  //console.log(`Rodando na porta ${port}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './app.js';
 
 const port = process.env.APP_PORT;
-app.listen(port);
+
+app.listen(port, () => {
+  //console.log(`Rodando na porta ${port}`);
+});
